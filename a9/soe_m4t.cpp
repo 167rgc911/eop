95c43ec711d1ecc37fb51c113b88ed9df09ce70b
rgc <74342662+167rgc911@users.noreply.github.com>
2024-04-22 21:02:07 +0900

    update; templated, generic programming

---
+++ a9/soe_m4t.cpp
@@ -0,0 +1,69 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  soe_m4t.cpp
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  04/22/24 20:57:50
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  rgc (),
+ *   Organization:
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+auto
+half (int n) -> int
+{
+  return (n / 2);
+}
+
+auto
+odd (int n) -> int
+{
+  return (n % 2);
+}
+
+template <typename N, typename A>
+constexpr auto
+multiply_acc4 (A r, N n, A a) -> int
+{
+  while (true)
+    {
+      if (odd (n))
+        {
+          r = r + a;
+          if (n == 1)
+            return r;
+        }
+      n = half (n);
+      a = a + a;
+    }
+}
+
+constexpr auto
+multiply4 (int n, int a) -> int
+{
+  while (!odd (n))
+    {
+      a = a + a;
+      n = half (n);
+    }
+  if (n == 1)
+    return a;
+  // even(n - 1) => n - 1 != 1
+  return multiply_acc4 (a, half (n - 1), a + a);
+}
+
+int
+main ()
+{
+  std::cout << multiply4 (41, 59) << '\n';
+  return 0;
+}
