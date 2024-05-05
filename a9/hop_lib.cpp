f9183e664ded1b093a8260af534452da753cfa9d
rgc <74342662+167rgc911@users.noreply.github.com>
2024-05-05 10:03:27 +0900

    clang-format -style=GNU

--- a9/hop_lib.cpp
+++ a9/hop_lib.cpp
@@ -3,7 +3,7 @@
  *
  *       Filename:  hop_lib.cpp
  *
- *    Description:  
+ *    Description:
  *
  *        Version:  1.0
  *        Created:  05/05/24 09:40:15
@@ -11,7 +11,7 @@
  *       Compiler:  gcc
  *
  *         Author:  rgc (), sessyargc.jp@gmail.com
- *   Organization:  
+ *   Organization:
  *
  * SPDX-License-Identifier: MIT
  * =====================================================================================
@@ -22,27 +22,36 @@
 #include <iostream>
 #include <vector>
 
-#include "soe_lib.h"
 #include "hop_lib.h"
+#include "soe_lib.h"
 
 int
 main ()
 {
-  std::vector<int> n(10);
-  std::iota(n.begin (), n.end (), 69);
-  [n]() { for (auto v : n) std::cout << v << ", "; } ();
+  std::vector<int> n (10);
+  std::iota (n.begin (), n.end (), 69);
+  [n] () {
+    for (auto v : n)
+      std::cout << v << ", ";
+  }();
 
   auto m = std::distance (n.begin (), n.end ()) / 2;
 
   std::cout << '\n';
 
-  Algo::gries_mills_rotate (n.begin(), n.begin() + m, n.end());
-  [n]() { for (auto v : n) std::cout << v << ", "; } ();
+  Algo::gries_mills_rotate (n.begin (), n.begin () + m, n.end ());
+  [n] () {
+    for (auto v : n)
+      std::cout << v << ", ";
+  }();
 
   std::cout << '\n';
 
-  std::rotate (n.begin(), n.begin() + m, n.end());
-  [n]() { for (auto v : n) std::cout << v << ", "; } ();
+  std::rotate (n.begin (), n.begin () + m, n.end ());
+  [n] () {
+    for (auto v : n)
+      std::cout << v << ", ";
+  }();
 
   std::cout << '\n';
 
